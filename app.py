cf766ba2acb81b86a1e709186e48ce18484767ae
takahiro <tkhr.corei7@gmail.com>
2018-11-13 01:54:41 +0900

    少しマシになった

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 #-*- coding: utf-8 -*-
 
-from flask import Flask, render_template, jsonify, request, url_for
-import json, sys, pprint, time, random, hashlib
+from flask import Flask, render_template, request, url_for
+import json, time, random, hashlib
 from datetime import datetime
-#sys.path.append("./mymodules")
 
 app = Flask(__name__)
 
@@ -26,10 +25,15 @@ def ReadData(_file_name = 'test'):
 #golobal variables
 file_name = "test"
 data_push_link = "fff2d2127188a272e7d87f9f5396e7d7"
+show_rename_qr_code_link = "77da6549b9ab3200490b2ed4d2a502d5"
 ranking = ReadData(file_name)
+renameble_score = 100
+
+front_url = "http://127.0.0.1:5000"
+rename_url_full = front_url + "/rename/"
 
 
-@app.route("/push_data/" + data_push_link, methods=["GET"])
+@app.route("/push_data/" + data_push_link, methods=['GET'])
 def PushData():
     def ReturnRandomHash(solt = 44124):
         #本来のsoltとは違うかも
@@ -46,36 +50,43 @@ def PushData():
     result["usr_id"] = ReturnRandomHash(57294375)
     result["score"] = int(request.args.get("score"))
     result["time_stamp"] = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    result["name"] = str(request.args.get("name"))
+    result["name"] = "無名のスケーター"
     result["receved_time_unix"] = int(time.mktime(datetime.now().timetuple()))
-    result["entry_num"] = len(ranking)
     ranking = ReadData('test')
+    result["entry_num"] = len(ranking)
     ranking.append(result)
     ranking = SortRanking(ranking)
     SaveData(ranking, 'test')
-    return render_template("received_msg.html")
+    return "receved data"
 
-@app.route("/show_rename_qr_code")
+@app.route("/show_rename_qr_code/" + show_rename_qr_code_link)
 def ShowRenameQrCode():
-    rename_url = "/rename/"
-
-    return render_template("show_rename_qr_code.html", rename_url = rename_url)
+    global ranking
+    global  rename_url_full
+    ranking_len = len(ranking)
+    for i in range(ranking_len):
+        #一番最後に入ってきたプレイヤー情報のエントリー番号 == ranking配列の長さ
+        if ranking[i]["entry_num"] == ranking_len-1:
+            break    
+    if (ranking[i]["score"] >= renameble_score):
+        rename_url_full = front_url + "/rename/" + ranking[i]["rename_url"]
+    return render_template("show_rename_qr_code.html", rename_url_full = rename_url_full)
 
 @app.route('/rename/<rename_url>')
 def rename(rename_url):
+    global ranking
     print(rename_url)
     for i in range(len(ranking)):
         if ranking[i]["rename_url"] == rename_url:
             break
     if (ranking[i]["rename_url"] == rename_url and ranking[i]["name_edited"] == 0):
-        #return render_template("rename_resutl.html", result_data=result_data)
-        return "valid page!!!"
+        return render_template("rename.html", data = ranking[i])
     else:
-        #return render_template("rename_error.html")
-        return "ERROR!!!"
+        return render_template("rename_error.html")
 
 @app.route('/rename/<rename_url>/result', methods=['GET'])
-def rename_result(rename_url):
+def RenameResult(rename_url):
+    global ranking
     print(rename_url)
     for i in range(len(ranking)):
         if ranking[i]["rename_url"] == rename_url:
@@ -84,30 +95,28 @@ def rename_result(rename_url):
         ranking[i]["name"] = str(request.args.get("name"))
         ranking[i]["name_edited"] += 1
         ranking[i]["renamed_time"] = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        result_data = ranking[i]
         SaveData(ranking)
-        #return render_template("rename_resutl.html", result_data=result_data)
-        return "Success!!!"
+        return render_template("rename_result.html", data = ranking[i])
     else:
-        #return render_template("rename_error.html")
-        return "ERROR!!!"
+        return render_template("rename_result_error.html", data = ranking[i])
 
 @app.route("/")
 def TopPage():
     ranking = ReadData('test')
-    name = "hoge"
     return render_template("top_page.html", ranking = ranking)
 
 
 @app.route("/ranking15")
-def ShowTopPage():
-    ranking = ReadData('test')
+def Ranking15():
+    global ranking
     return render_template("ranking15.html", ranking=ranking)
 
 @app.route("/search_rank", methods=['GET'])
 @app.route("/ranking15/search_rank", methods=['GET'])
 def SearchRank():
-    return "serach rank"
+    global ranking
+    serch_result = ranking[0]
+    return render_template("search_rank.html", serch_result = serch_result)
 
 
 if __name__ == '__main__':
